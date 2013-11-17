de9b9970c8dab621b96ebd98a386bb79c90ae4d6
James Cole <jemilco@gmail.com>
2013-11-17 14:04:36 -0500

    update navigation options

--- templates/common/common-navigation.php
+++ templates/common/common-navigation.php
@@ -1,1 +1,8 @@
-<?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+<?php
+wp_link_pages(
+  array( 'before' => '<nav class="page-nav"><p>' . __( 'Pages:', 'dawn' ),
+         'after'  => '</p></nav>'
+
+  )
+);
+?>
